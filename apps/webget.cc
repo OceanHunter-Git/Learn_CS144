56d1f2841865cdc3950b9c24d2385d6e068ec9d1
OceanHunter-Git <13056790885@163.com>
2025-06-26 22:36:39 +0800

    get_URL

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,6 +9,31 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
+	TCPSocket socket;
+	Address address(host, "http");
+
+	socket.connect(address);
+
+	const string request = 
+  	"GET " + path + " HTTP/1.1\r\n"
+    "Host: " + host + "\r\n"
+		"Connection: close\r\n"
+		"\r\n";
+
+	socket.write(request);
+
+	string res;
+    while (true) {
+      string buffer;
+      socket.read(buffer);
+      if (socket.eof()) {
+        break;
+      }
+			res += buffer;
+      }
+
+  cout << res;
+
   cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
   cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
